152fbb22b2bced044db4adfc26597b91a57865f1
Denis Vanoni <dex.vanoni@gmail.com>
2017-06-18 14:09:14 -0400

    Testes de Seções OK

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -12,31 +12,30 @@
   <div class="jumbotron">
     <div class="col-md-offset-2">
       <h2>Meu dashboard</h2>
-      <p>Todas as opções do sistema de Ordens de Serviço para solicitação de diárias você encontrará aqui!</p>
-      <p><a data-toggle="collapse" data-target="#options" class="btn btn-default" role="button">Minhas Opções</a></p>
       <p>Usuário: {{ Session::get('grad') }} {{ Session::get('pesnguerra') }}</p>
+      <p><a data-toggle="collapse" data-target="#options" class="btn btn-primary" role="button">Minhas Opções</a></p>
     </div>
   </div>
   <div class="collapse" id="options">
     <div class="row">
       <div class="col-md-2 col-xs-offset-2">
         <br>
-        <a href="{{ route('ficha.index') }}" class="btn btn-info"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Minhas OS</a>
-      </div>
-      <div class="col-md-4 col-xs-offset-4">
-        Ordens de Serviço de outro militar
-        {{ Form::open(array('action'=>'LogInController@outro', 'class' => 'form-horizontal', 'method' => 'post')) }}
+      <a href="{{ route('ficha.index') }}" class="btn btn-info"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Minhas OS</a>
+    </div>
+    <div class="col-md-4 col-xs-offset-4">
+      Ordens de Serviço de outro militar
+        {!! Form::open(array('route' => 'outro', 'method' => 'POST')) !!}
         {!! Form::text('outro_militar', null, array('size'=>'15', 'id'=>'outro_militar', 'placeholder' => 'SARAM')) !!}
         {{ Form::submit('Ir', array('class'=>'btn btn-primary')) }}
         {!! Form::close() !!}
-      </div>
-    </div>
   </div>
-  <hr>
-  <div class="container">
-    <center><img src="/bst/brasao.png" class="img-responsive" alt="" height="150em" width="150em" /></center>
-    <center><h2>@TIC</h2></center>
-  </div>
-  <script src="/bst/js/bootstrap.min.js"></script>
+</div>
+</div>
+<hr>
+<div class="container">
+  <center><img src="/bst/brasao.png" class="img-responsive" alt="" height="150em" width="150em" /></center>
+  <center><h2>@TIC</h2></center>
+</div>
+<script src="/bst/js/bootstrap.min.js"></script>
 </body>
 </html>
